c475b3809e9bb8fb5c31ebd580284acbc3212a24
jiceberg <jisenberg3@gatech.edu>
2022-03-04 00:09:55 -0500

    fix lack of control for double letters

--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
 from words import answers, words
 import numpy as np
+from collections import Counter
 
 class Game:
 
@@ -12,12 +13,15 @@ class Game:
             return []
         
         result = ['\u2B1B' for _ in range(5)]
+        cnt = Counter(self.solution)
         for i in range(5):
             guess_letter, actual_letter = word[i], self.solution[i]
             if guess_letter == actual_letter:
                 result[i] = '\U0001f7e9'
-            elif guess_letter in self.solution:
+                cnt[guess_letter] -= 1
+            elif cnt[guess_letter] and cnt[guess_letter] > 0:
                 result[i] = '\U0001f7e8'
+                cnt[guess_letter] -= 1
         return result
 
     def get_solution(self):
